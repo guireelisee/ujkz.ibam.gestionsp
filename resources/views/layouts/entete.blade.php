a86f2be500dd087252f3b2b32a6360beb4f4a997
guireelisee <guireelisee@gmail.com>
2021-06-11 14:11:12 +0000

    entete des actes cad logo ibam, uo, contacts...

--- resources/views/layouts/entete.blade.php
+++ resources/views/layouts/entete.blade.php
@@ -14,47 +14,86 @@
 
 
 
-<body class="hold-transition sidebar-mini">
+<body style="background-color: white">
 
-  <div class="wrapper">
+  <div class="wrapper" style="font-weight: bolder">
     <!-- Content Wrapper. Contains page content -->
-
-    <div class="container" style="width: 78%;">
-        <div class="row">
-            <div class="col-md-9">
-                <div class="container">
-                    <div class="row">
-                        <div class="col img-thumbnail me-4">
-                        <img src="{{ asset('img/logo/ibam.png') }}" style="width: 50%" alt="" class="">
-                    </div>
-                    <div class="col img-thumbnail">
-                        <img src="{{ asset('img/logo/UO.jpg') }}" style="width: 32%" alt="" class="r">
-                    </div>
-                    </div>
-
-                        <p>SOMGANDE, OUAGADOUGOU <br>
-                        Adresse<span>&nbsp;</span>:  BP 202 Ouaga <br>
-                        Tel (+226): 62 82 64 88, 66 59 48 66 , 66 68 03 54 <br>
-                        Fax (+226): 25 35 67 62
-                        </p>
-                    <hr width="1000">
-                    <div class="img" style=" margin-left: 15px;">
-                        <p>Coordonnées du client<br>
-                        Tel:<br>
-                        </p>
-                    </div>
-
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="container" style="margin-top: 200px;">
-                    <h4>FACTURE N°</h4>
-                    <h5>Date: </h5>
-                </div>
-            </div>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <span>
+            <em>KGTAN/ <br></em>
+            MINISTERE DE L’ENSEIGNEMENT SUPERIEUR, <br>
+            DE LA RECHERCHE SCIENTIFIQUE <br>
+            ET DE L’INNOVATION <br>
+            -=-=-=-=-=-=-=- <br>
+            SECRETARIAT GENERAL <br>
+            -=-=-=-=-=-=- <br>
+            UNIVERSITE JOSEPH KI-ZERBO <br>
+            -=-=-=--=-==-=- <br>
+          </span>
         </div>
+        <div class="col">
+          <img class="mt-5" src="{{ asset('img/logo/UO.jpg') }}" width= "150" height="150" alt="">
+        </div>
+        <div class="col">
+          <span>
+            BURKINA FASO <br>
+            -=-=-=-=-=- <br>
+            Unité – Progrès – Justice !
+          </span>
+        </div>
+      </div>
     </div>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <img class="ms-5" src="{{ asset('img/logo/ibam.png') }}" width= "150" height="90" alt="">
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <span>
+            Institut Burkinabè des Arts et Métiers <br>
+            03 BP 7021 Ouagadougou 03 <br>
+            Tél. : (226) 25-35-67-31/62 <br>
+            Fax. : (226) 25-30-72-42 <br>
+            Télex : 5270 BF
+          </span>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col">
+          <span>
+            <div style="margin: -10% 0% 0% 40%">
+              <span>
+                Ouagadougou, le <br>
+              </span>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+    <br><br>
+     <div class="container">
+      <div class="row text-center" style="margin: 0% 0% 0% 3%">
+          <span>
+            N°<?=date('Y');?>-________________/MESRSI/SG/UJKZ/IBAM/D
+          </span>
+        </div>
+      </div>
+    </div>
+    @yield('destinataire')
   </div>
+  @yield('corps')
 
   {{-- <script type="text/javascript">window.print();</script> --}}
 
