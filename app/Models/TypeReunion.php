7d1ebdb245b66d7f8a7315c0bfb3e907ff996d87
guireelisee <guireelisee@gmail.com>
2021-06-23 20:39:52 +0000

    fix types

--- app/Models/TypeReunion.php
+++ app/Models/TypeReunion.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class TypeReunion extends Model
 {
     use HasFactory;
-    protected $fillable = ['idTR'];
+    protected $guarded = ['idTR'];
 
     protected $primaryKey = 'idTR';
 }
